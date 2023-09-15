00b8da3dce102b6a3fffcc6d82541958bef22306
Georg <georgdonner@posteo.de>
2023-09-15 13:21:17 +0200

    Add setting for grid size

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   let words = [];
   let word = '';
   let boards = '1';
+  let grid = '5';
 
   if (localStorage.getItem('words')) {
     words = JSON.parse(localStorage.getItem('words'));
@@ -35,28 +36,29 @@
       <label for="word">Neues Wort: </label>
       <input bind:value={word} type="text" name="word" id="word">
     </form>
+
+    <button on:click={handleClear}>Wörter löschen</button>
+
+    <br style="margin-bottom: 1.5rem;">
   
     <label for="boards">Anzahl Bretter: </label>
     <input bind:value={boards} type="number" name="boards" id="boards" min="1">
 
-    <button on:click={handleClear}>Wörter löschen</button>
+    <label for="grid">Anzahl Spalten: </label>
+    <input bind:value={grid} type="number" name="grid" id="grid" min="2">
   </div>
 
-  <div class="bingo cut">
+  <div class="bingo cut" style="grid-template-columns: repeat({grid}, 1fr);">
     {#each words as word}
       <div>{word}</div>
     {/each}
   </div>
 
   {#each Array(Number(boards)) as i}
-    <div class="bingo board">
+    <div class="bingo board" style="grid-template-columns: repeat({grid}, 1fr);">
       {#each shuffle(words) as word}
         <div>{word}</div>
       {/each}
     </div>
   {/each}
 </main>
-
-<style>
-
-</style>
