f78bae3516e29cd90eb7d099e1cc0d004fac7427
Georg <georgdonner@posteo.de>
2023-09-15 13:53:08 +0200

    Prevent shuffling main words array

--- src/App.svelte
+++ src/App.svelte
@@ -40,12 +40,13 @@
     localStorage.setItem('words', JSON.stringify(words));
   };
 
-  const shuffle = (array) => { 
-    for (let i = array.length - 1; i > 0; i--) { 
+  const shuffle = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) { 
       const j = Math.floor(Math.random() * (i + 1)); 
-      [array[i], array[j]] = [array[j], array[i]]; 
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; 
     } 
-    return array; 
+    return newArray; 
   };
 </script>
 
